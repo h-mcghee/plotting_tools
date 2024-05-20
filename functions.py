Fixed bug with tof calibration (masking)

functions.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout,QSlider, QWidget, QGridLayout,QSizePolicy, QCheckBox, QLineEdit, QPushButton, QSpinBox,QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout,QSlider, QWidget, QGridLayout,QSizePolicy, QCheckBox, QLineEdit, QPushButton, QSpinBox,QFileDialog, QMessageBox
 from PyQt5.QtCore import Qt
 
 
@@ -38,6 +38,8 @@ class MainWindow(QMainWindow):
         self.scale = 'linear'
 
         self.is_checked = False
+
+        self.error = []
 
         self.setWindowTitle("2D Array Plotter")
         self.setGeometry(200, 200, 1000, 1500)
@@ -77,7 +79,7 @@ class MainWindow(QMainWindow):
         self.save_button.clicked.connect(self.save)
         layout.addWidget(self.save_button,2,1)
 
-        self.selected_values = None
+        self.selected_values = []
 
         # self.log_checkbox = QCheckBox("Log scale")
         # self.log_checkbox.stateChanged.connect(self.log_scale)
@@ -173,15 +175,17 @@ class MainWindow(QMainWindow):
             super().keyPressEvent(event)
 
     def update_xplot(self):
-        x_index = self.x_slider.value()
+        x_index = self.x_slider.value() 
         self.ax_original.clear()
         if self.is_checked == True:
             self.selected_values = np.sum(self.matrix[:, x_index:x_index+self.spin_box.value()],axis=1)
+            self.error = np.sqrt(2) * np.sqrt(np.sum(self.og_matrix[:, x_index:x_index+self.spin_box.value()],axis=1))
             self.ax_original.axvline(self.x[x_index], color='red')
             self.ax_original.axvline(self.x[x_index+self.spin_box.value()-1], color='red')
 
         else:
             self.selected_values = self.matrix[:, x_index]
+            self.error = np.sqrt(2) * np.sqrt(self.og_matrix[:, x_index])
             self.ax_original.axvline(self.x[x_index], color='red')
 
 
@@ -198,9 +202,10 @@ class MainWindow(QMainWindow):
         # Update selected Y values plot
         self.ax_selected.clear()
         self.ax_selected.plot(self.y,self.selected_values)
-        self.ax_selected.fill_between(self.y,self.selected_values - np.sqrt(self.selected_values),self.selected_values + np.sqrt(self.selected_values),alpha=0.5)
+        
+        self.ax_selected.fill_between(self.y,self.selected_values - self.error, self.selected_values + self.error,alpha=0.5)
         if self.is_checked == True:
-            self.ax_selected.set_title(f"Sum of Y values for X={self.x[x_index]} to {self.x[x_index+self.spin_box.value()-1]}")
+            self.ax_selected.set_title(f"Sum of Y values for X={self.x[x_index]:.3f} to {self.x[x_index+self.spin_box.value()-1]:.3f}")
         else:
             self.ax_selected.set_title(f"Selected Y values for X={self.x[x_index]}")
         self.ax_selected.set_xlabel("Y index")
@@ -244,14 +249,17 @@ class MainWindow(QMainWindow):
 
     def bkg_sub(self,state):
         bkg = np.mean(self.matrix[0:3,:],axis=0)
-        if state == Qt.Checked:
-            self.matrix = self.matrix - bkg
-            self.update_xplot()
-            self.update_yplot()
-        else:
-            self.matrix = self.og_matrix
-            self.update_xplot()
-            self.update_yplot()
+        try:
+            if state == Qt.Checked:
+                self.matrix = self.matrix - bkg
+                self.update_xplot()
+                self.update_yplot()
+            else:
+                self.matrix = self.og_matrix
+                self.update_xplot()
+                self.update_yplot()
+        except Exception as e:
+            QMessageBox.critical(None, "Error", str(e), QMessageBox.Ok)
 
     def apply_x_limits(self):
         self.xlims = [float(self.xmin_lineedit.text()),float(self.xmax_lineedit.text())]
@@ -260,19 +268,28 @@ class MainWindow(QMainWindow):
         
 
     def tof_calib(self,state):
-        if state == Qt.Checked:
-            self.x = tof2energy(self.x,2.164476933102369571e+01,3.278435274171569436e-01,3.444852703175022270e+03,0.000000000000000000e+00)
-            mask = self.x > 0 
-            self.x = self.x[mask]
-            self.matrix = self.matrix[:,mask]
-            self.xlims = [min(self.x),max(self.x)]
-            self.update_xplot()
-            self.update_yplot()
-        else:
-            self.x = self.og_x
-            self.xlims = [min(self.x),max(self.x)]
-            self.update_xplot()
-            self.update_yplot()
+        try:
+            self.x_slider.setValue(0)
+            if state == Qt.Checked:
+                
+                self.x = tof2energy(self.x,2.164476933102369571e+01,3.278435274171569436e-01,3.444852703175022270e+03,0.000000000000000000e+00)    
+                min_index = np.argmin(self.x)
+                mask = (np.arange(self.x.shape[0]) >= min_index) & (self.x > 0)
+                self.x = self.x[mask]
+                self.matrix = self.matrix[:,mask]
+                self.xlims = [min(self.x),max(self.x)]
+
+                self.update_xplot()
+                self.update_yplot()
+            else:
+                self.x = self.og_x
+                self.matrix = self.og_matrix
+                self.xlims = [min(self.x),max(self.x)]
+                self.update_xplot()
+                self.update_yplot()
+
+        except Exception as e:
+            QMessageBox.critical(None, "Error", str(e), QMessageBox.Ok)
 
     def set_state(self,state):
         if state == Qt.Checked: